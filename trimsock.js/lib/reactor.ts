ddd04a573b2e5f3904d9088ac12a0dff80498f37
Tamás Gálffy <ezittgtx@gmail.com>
2025-07-15 15:36:08 +0200

    reactor

--- trimsock.js/lib/reactor.ts
+++ trimsock.js/lib/reactor.ts
@@ -1,3 +1,4 @@
+import type { SocketHandler } from "bun";
 import type { Command } from "./command";
 import { isCommand, Trimsock } from "./trimsock";
 
@@ -8,31 +9,82 @@ export interface TrimsockResponse {
   send(command: Command): void;
 }
 
-export class Reactor {
+export abstract class Reactor<T> {
   private handlers: Map<string, CommandHandler> = new Map();
 
   constructor(
     private trimsock: Trimsock = new Trimsock().withConventions()
   ) {}
 
-  public on(commandName: string, handler: CommandHandler): Reactor {
+  public on(commandName: string, handler: CommandHandler): this {
     this.handlers.set(commandName, handler);
 
     return this;
   }
 
-  public ingest(data: Buffer, sink: OutputSink) {
+  public ingest(data: Buffer, source: T) {
     this.trimsock.ingest(data)
       .filter(it => isCommand(it))
-      .forEach(command => this.handle(command as Command, sink));
+      .forEach(command => this.handle(command as Command, source));
   }
 
-  private handle(command: Command, sink: OutputSink) {
+  public send(target: T, command: Command) {
+    this.write(this.trimsock.asString(command), target);
+  }
+
+  protected abstract write(data: string, target: T): void;
+
+  private handle(command: Command, source: T) {
     const handler = this.handlers.get(command.name);
     if (handler) {
       handler(command, {
-        send: (cmd) => sink(this.trimsock.asString(cmd))
+        send: (cmd) => this.write(this.trimsock.asString(cmd), source)
       });
+    }
+  }
+}
+
+export class SocketReactor<SocketData = undefined> extends Reactor<Bun.Socket<SocketData>> {
+  public listen(options: Bun.TCPSocketListenOptions<SocketData>): Bun.TCPSocketListener<SocketData> {
+    return Bun.listen({
+      ...options,
+      ...this.wrapHandlers(options)
+    })
+  }
+
+  public connect(options: Bun.TCPSocketConnectOptions<SocketData>): Promise<Bun.Socket<SocketData>> {
+    return Bun.connect({
+      ...options,
+      ...this.wrapHandlers(options)
+    });
+  }
+
+  protected write(data: string, target: Bun.Socket<SocketData>): void {
+    target.write(data);
+  }
+
+  private wrapHandlers(options: Bun.SocketOptions<SocketData>): Bun.SocketOptions<SocketData> {
+    const baseHandlers: SocketHandler<SocketData> = options.socket ?? {};
+
+    return {
+      socket: {
+        data: (socket, data) => {
+          baseHandlers.data?.call(baseHandlers.data, socket, data)
+          this.ingest(data, socket)
+        }, 
+        open: (socket) => {
+          baseHandlers.open?.call(baseHandlers.data, socket)
+        },
+        close: (socket, error) => {
+          baseHandlers.close?.call(baseHandlers.data, socket, error)
+        },
+        drain: (socket) => {
+          baseHandlers.drain?.call(baseHandlers.data, socket)
+        },
+        error: (socket, error) => {
+          baseHandlers.error?.call(baseHandlers.data, socket, error)
+        },
+      }
     }
   }
 }
--- trimsock.js/server.ts
+++ trimsock.js/server.ts
@@ -1,36 +1,33 @@
-import { Reactor } from "@lib/reactor";
+import { SocketReactor } from "@lib/reactor";
 import { randomUUIDv7 } from "bun";
 
 const sessionIds: Map<Bun.Socket, string> = new Map();
 
-const reactor = new Reactor()
+const reactor = new SocketReactor()
   .on('echo', (cmd, response) => response.send(cmd))
   .on('info', (cmd, response) => response.send({ name: 'info', data: Buffer.from('trimsock reactor', 'ascii'), isRaw: false }))
 
-Bun.listen({
+reactor.listen({
   hostname: "localhost",
   port: 8890,
   socket: {
-    data(socket, data) {
-      reactor.ingest(data, output => socket.write(output))
-    }, 
     open(socket) {
       const sessionId = randomUUIDv7();
       sessionIds.set(socket, sessionId);
 
       console.log("Created session ", sessionId);
+      reactor.send(socket, { name: "greet", data: Buffer.from(sessionId, "ascii"), isRaw: false })
     }, 
     close(socket, error) {
       const sessionId = sessionIds.get(socket)
       sessionIds.delete(socket)
       console.log("Closed session ", sessionId, error)
     }, 
-    drain(socket) {}, 
     error(socket, error) {
       const sessionId = sessionIds.get(socket)
       console.error("Error in session ", sessionId, error)
     }, 
   }
-})
+});
 
 console.log("Listening");
