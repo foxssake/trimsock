ffa9dff9189acab9d9b6b97fc40331a557e879a5
Tamás Gálffy <ezittgtx@gmail.com>
2025-07-29 23:03:38 +0200

    feat(js): Add node adapter package (#27)

---
+++ trimsock.js/examples/node-server/index.ts
@@ -0,0 +1,25 @@
+import type * as net from "node:net";
+import { makeDefaultIdGenerator } from "@foxssake/trimsock-js";
+import { NodeSocketReactor } from "@foxssake/trimsock-node";
+
+const port = 8890;
+const sessionIds: Map<net.Socket, string> = new Map();
+
+const generateSessionId = makeDefaultIdGenerator(4);
+
+new NodeSocketReactor()
+  .on("info", (_, xchg) =>
+    xchg.replyOrSend({ name: "info", data: "trimsock on node" }),
+  )
+  .on("whoami", (_, xchg) =>
+    xchg.replyOrSend({
+      name: "whoami",
+      data: sessionIds.get(xchg.source) || "???",
+    }),
+  )
+  .serve({}, (socket) => {
+    const sessionId = generateSessionId();
+    sessionIds.set(socket, sessionId);
+    console.log("New session", sessionId);
+  })
+  .listen(port, () => console.log("Listening on port", port));
