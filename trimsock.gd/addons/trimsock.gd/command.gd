54a403454227f351249273fff3345810716084f7
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-06 21:32:59 +0200

    command + line reader + tests

---
+++ trimsock.gd/addons/trimsock.gd/command.gd
@@ -0,0 +1,51 @@
+extends RefCounted
+class_name TrimsockCommand
+
+class Chunk:
+	var text: String
+	var is_quoted: bool
+
+class Pair:
+	var key: String
+	var value: String
+
+enum Type {
+	SIMPLE,
+	REQUEST,
+	SUCCESS_RESPONSE,
+	ERROR_RESPONSE,
+	STREAM_CHUNK,
+	STREAM_FINSIH
+}
+
+# Core properties
+var text: String = ""
+var chunks: Array[Chunk] = []
+var raw: PackedByteArray
+
+# Multiparam
+var params: Array[String]
+
+# Key-value pairs
+var kv_pairs: Array[Pair]
+var kv_map: Dictionary
+
+# Request-response + Stream
+var exchange_id: String
+var type: Type = Type.SIMPLE
+
+
+func is_request() -> bool:
+	return type == Type.REQUEST
+
+func is_success() -> bool:
+	return type == Type.SUCCESS_RESPONSE
+
+func is_error() -> bool:
+	return type == Type.ERROR_RESPONSE
+
+func is_stream_chunk() -> bool:
+	return type == Type.STREAM_CHUNK
+
+func is_stream_end() -> bool:
+	return type == Type.STREAM_FINSIH
