efd78453f7ac74c14ccfb1535964e91dc47d48fc
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-08 22:52:32 +0200

    feat(gd): Serialize commands (#56)
    Refs #29 
    Refs #53

--- trimsock.gd/addons/trimsock.gd/command.gd
+++ trimsock.gd/addons/trimsock.gd/command.gd
@@ -36,12 +36,67 @@ var kv_map: Dictionary
 var exchange_id: String
 var type: Type = Type.SIMPLE
 
+static func from_buffer(name: String, data: PackedByteArray) -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.is_raw = true
+	command.raw = data
+	return command
+
+static func simple(name: String) -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	return command
+
+static func request(name: String, exchange_id: String = "") -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	command.type = Type.REQUEST
+	command.exchange_id = exchange_id
+	return command
+
+static func success_response(name: String, exchange_id: String = "") -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	command.type = Type.SUCCESS_RESPONSE
+	command.exchange_id = exchange_id
+	return command
+
+static func error_response(name: String, exchange_id: String = "") -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	command.type = Type.ERROR_RESPONSE
+	command.exchange_id = exchange_id
+	return command
+
+static func stream_chunk(name: String, exchange_id: String = "") -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	command.type = Type.STREAM_CHUNK
+	command.exchange_id = exchange_id
+	return command
+
+static func stream_finish(name: String, exchange_id: String = "") -> TrimsockCommand:
+	var command := TrimsockCommand.new()
+	command.name = name
+	command.type = Type.STREAM_FINISH
+	command.exchange_id = exchange_id
+	return command
 
 static func unescape(what: String) -> String:
 	return (what
 		.replace("\\n", "\n")
 		.replace("\\r", "\r")
 		.replace("\\\"", "\"")
+	)
+
+static func escape_quoted(what: String) -> String:
+	return what.replace("\"", "\\\"")
+
+static func escape_unquoted(what: String) -> String:
+	return (what
+		.replace("\n", "\\n")
+		.replace("\r", "\\r")
+		.replace("\"", "\\\"")
 	)
 
 static func pair_of(key: String, value: String) -> Pair:
@@ -75,3 +130,169 @@ func is_stream_chunk() -> bool:
 
 func is_stream_end() -> bool:
 	return type == Type.STREAM_FINISH
+
+func is_empty() -> bool:
+	if is_raw:
+		return raw.is_empty()
+	else:
+		return text.is_empty() and chunks.is_empty() and params.is_empty() and kv_pairs.is_empty() and kv_map.is_empty()
+
+func with_name(p_name: String) -> TrimsockCommand:
+	name = p_name
+	return self
+
+func with_text(p_text: String) -> TrimsockCommand:
+	text = p_text
+	return self
+
+func with_chunks(p_chunks: Array[Chunk]) -> TrimsockCommand:
+	chunks += p_chunks
+	return self
+
+func as_raw() -> TrimsockCommand:
+	is_raw = true
+	text = ""
+	chunks = []
+	return self
+
+func with_data(data: PackedByteArray) -> TrimsockCommand:
+	as_raw()
+	raw = data
+	return self
+
+func with_params(p_params: Array[String]) -> TrimsockCommand:
+	params += p_params
+	return self
+
+func with_kv_pairs(p_kv_pairs: Array[Pair]) -> TrimsockCommand:
+	kv_pairs += p_kv_pairs
+	for pair in kv_pairs:
+		kv_map[pair.key] = pair.value
+	return self
+
+func with_kv_map(p_kv_map: Dictionary) -> TrimsockCommand:
+	for key in p_kv_map:
+		var value = p_kv_map[key]
+		kv_pairs.append(pair_of(key, value))
+	kv_map.merge(p_kv_map, true)
+	return self
+
+func with_exchange_id(p_exchange_id: String) -> TrimsockCommand:
+	exchange_id = p_exchange_id
+	return self
+
+func as_request() -> TrimsockCommand:
+	type = Type.REQUEST
+	return self
+
+func as_success_response() -> TrimsockCommand:
+	type = Type.SUCCESS_RESPONSE
+	return self
+
+func as_error_response() -> TrimsockCommand:
+	type = Type.ERROR_RESPONSE
+	return self
+
+func as_stream() -> TrimsockCommand:
+	type = Type.STREAM_FINISH if is_empty() else Type.STREAM_CHUNK
+	return self
+
+func serialize() -> PackedByteArray:
+	var out := PackedByteArray()
+	serialize_to_array(out)
+	return out
+
+func serialize_to_array(out: PackedByteArray) -> void:
+	var buffer := StreamPeerBuffer.new()
+	serialize_to_buffer(buffer)
+	out.append_array(buffer.data_array)
+
+func serialize_to_buffer(out: StreamPeerBuffer) -> void:
+	# Add raw marker
+	if is_raw:
+		out.put_8(13) # \r
+
+	# Add name
+	out.put_data(_escape_name(name).to_utf8_buffer())
+
+	# Add separator if request / stream
+	match type:
+		Type.REQUEST: out.put_u8(_ord("?"))
+		Type.SUCCESS_RESPONSE: out.put_u8(_ord("."))
+		Type.ERROR_RESPONSE: out.put_u8(_ord("!"))
+		Type.STREAM_CHUNK, Type.STREAM_FINISH:
+			out.put_u8(_ord("|"))
+
+	# Add ID
+	if type != Type.SIMPLE:
+		out.put_data(exchange_id.to_utf8_buffer())
+
+	# Short-circuit on empty command
+	if is_empty() and not is_raw:
+		out.put_u8(_ord("\n"))
+		return
+	
+	# Space after name
+	out.put_u8(_ord(" "))
+
+	# Short-circuit if raw
+	if is_raw:
+		out.put_data(str(raw.size()).to_ascii_buffer())
+		out.put_u8(_ord("\n"))
+		out.put_data(raw)
+		out.put_u8(_ord("\n"))
+		return
+
+	# Print content
+	if not chunks.is_empty():
+		# Prefer chunks, if available
+		for chunk in chunks:
+			if chunk.is_quoted:
+				out.put_u8(_ord("\""))
+				out.put_data(_quoted_chunk(chunk.text).to_utf8_buffer())
+				out.put_u8(_ord("\""))
+			else:
+				out.put_data(_unquoted_chunk(chunk.text).to_utf8_buffer())
+	elif not kv_pairs.is_empty() or not kv_map.is_empty() or not params.is_empty():
+		# Fall back to params if no chunks
+		var tokens := PackedStringArray()
+		
+		# Print params first
+		for param in params:
+			tokens.append(_autoquoted_chunk(param))
+		
+		# Print kv-params, either from `kv_pairs`, or `kv_map`
+		if not kv_pairs.is_empty():
+			for pair in kv_pairs:
+				tokens.append(_autoquoted_chunk(pair.key) + "=" + _autoquoted_chunk(pair.value))
+		else:
+			for key in kv_map:
+				var value = kv_map[key]
+				tokens.append(_autoquoted_chunk(key) + "=" + _autoquoted_chunk(value))
+
+		# Push to buffer
+		out.put_data(" ".join(tokens).to_utf8_buffer())
+	else:
+		# Use `text` as last resort
+		out.put_data(_autoquoted_chunk(text).to_utf8_buffer())
+
+	# Add closing NL
+	out.put_u8(_ord("\n"))
+
+func _ord(chr: String) -> int:
+	return chr.unicode_at(0)
+
+func _escape_name(what: String) -> String:
+	return _autoquoted_chunk(what)
+
+func _quoted_chunk(what: String) -> String:
+	return "\"%s\"" % [escape_quoted(what)]
+
+func _unquoted_chunk(what: String) -> String:
+	return escape_unquoted(what)
+
+func _autoquoted_chunk(what: String) -> String:
+	if what.contains(" "):
+		return _quoted_chunk(what)
+	else:
+		return _unquoted_chunk(what)
---
+++ trimsock.gd/tests/command.test.gd
@@ -0,0 +1,47 @@
+extends VestTest
+
+func get_suite_name():
+	return "Command"
+
+func suite():
+	define("serialize", func():
+		define("name", func():
+			check_serialized("simple name", TrimsockCommand.simple("command"), "command\n")
+			check_serialized("quote name", TrimsockCommand.simple("command name"), "\"command name\"\n")
+			check_serialized("raw", TrimsockCommand.simple("command").as_raw(), "\rcommand 0\n\n")
+			check_serialized("raw quoted", TrimsockCommand.simple("command name").as_raw(), "\r\"command name\" 0\n\n")
+		)
+		
+		define("request-response", func():
+			check_serialized("request", TrimsockCommand.request("command", "1234"), "command?1234\n")
+			check_serialized("success", TrimsockCommand.success_response("command", "1234"), "command.1234\n")
+			check_serialized("error", TrimsockCommand.error_response("command", "1234"), "command!1234\n")
+			check_serialized("stream", TrimsockCommand.stream_chunk("command", "1234"), "command|1234\n")
+		)
+		
+		define("multiparam", func():
+			check_serialized("simple params", TrimsockCommand.simple("command").with_params(["foo", "bar"]), "command foo bar\n")
+			check_serialized("quote params", TrimsockCommand.simple("command").with_params(["foo bar", "quix"]), "command \"foo bar\" quix\n")
+		)
+		
+		define("kv-params", func():
+			check_serialized("simple", TrimsockCommand.simple("command").with_kv_pairs([TrimsockCommand.pair_of("foo", "bar")]), "command foo=bar\n")
+			check_serialized("quoted", TrimsockCommand.simple("command").with_kv_pairs([TrimsockCommand.pair_of("foo bar", "quix baz")]), "command \"foo bar\"=\"quix baz\"\n")
+			check_serialized("key-quoted", TrimsockCommand.simple("command").with_kv_pairs([TrimsockCommand.pair_of("foo bar", "quix")]), "command \"foo bar\"=quix\n")
+			check_serialized("value-quoted", TrimsockCommand.simple("command").with_kv_pairs([TrimsockCommand.pair_of("foo", "quix baz")]), "command foo=\"quix baz\"\n")
+			check_serialized("with params", TrimsockCommand.simple("command").with_kv_pairs([TrimsockCommand.pair_of("foo", "bar")]).with_params(["foo", "bar"]), "command foo bar foo=bar\n")
+		)
+		
+		define("kv-map", func():
+			check_serialized("simple", TrimsockCommand.simple("command").with_kv_map({ "foo": "bar" }), "command foo=bar\n")
+			check_serialized("quoted", TrimsockCommand.simple("command").with_kv_map({ "foo bar": "quix baz"}), "command \"foo bar\"=\"quix baz\"\n")
+			check_serialized("key-quoted", TrimsockCommand.simple("command").with_kv_map({ "foo bar": "quix" }), "command \"foo bar\"=quix\n")
+			check_serialized("value-quoted", TrimsockCommand.simple("command").with_kv_map({ "foo": "quix baz"}), "command foo=\"quix baz\"\n")
+			check_serialized("with params", TrimsockCommand.simple("command").with_kv_map({ "foo": "bar" }).with_params(["foo", "bar"]), "command foo bar foo=bar\n")
+		)
+	)
+
+func check_serialized(name: String, command: TrimsockCommand, expected_line: String) -> void:
+	test(name, func():
+		expect_equal(command.serialize().get_string_from_utf8(), expected_line)
+	)
