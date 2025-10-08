d14c74bd2aeffb4496bd9b442070684eae060633
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-08 13:27:20 +0200

    cleanup + stream finish

--- trimsock.gd/addons/trimsock.gd/conventions.gd
+++ trimsock.gd/addons/trimsock.gd/conventions.gd
@@ -4,7 +4,6 @@ class_name _TrimsockConventions
 
 static func apply(command: TrimsockCommand) -> void:
 	parse_type(command)
-	parse_params(command)
 
 static func parse_type(command: TrimsockCommand) -> void:
 	var at := 0
@@ -28,54 +27,18 @@ static func parse_type(command: TrimsockCommand) -> void:
 		
 		at = command.name.find("|")
 		if at >= 0:
-			command.type = TrimsockCommand.Type.STREAM_CHUNK
+			if ((command.is_raw and command.raw.is_empty()) or command.text.is_empty()):
+				command.type = TrimsockCommand.Type.STREAM_FINSIH
+			else:
+				command.type = TrimsockCommand.Type.STREAM_CHUNK
 			break
 		return
-
-	# TODO: Distinguish STREAM_CHUNK and STREAM_FINISH
 
 	var name := command.name.substr(0, at)
 	var id := command.name.substr(at + 1)
 
 	command.name = name
 	command.exchange_id = id
-
-static func parse_params(command: TrimsockCommand) -> void:
-	if command.is_raw or command.chunks.is_empty():
-		return
-
-	var chunks := [] as Array[String]
-	for chunk in command.chunks:
-		if chunk.is_quoted:
-			# Quoted chunks go in verbatim
-			chunks.append(chunk.text)
-		else:
-			# Unquoted chunks are separated by spaces, and then each separated
-			# word is checked for equal signs
-			for word in chunk.text.split(" "):
-				var at := word.find("=")
-				if at >= 0:
-					chunks.append(word.substr(0, at))
-					chunks.append("=")
-					chunks.append(word.substr(at + 1))
-	
-	# Extract params and kv-pairs
-	for i in range(chunks.size()):
-		var chunk := chunks[i]
-		var prev := chunk[i-1] if i > 0 else ""
-		var next := chunk[i+1] if i < chunks.size() - 1 else ""
-		
-		if next == "=" or prev == "=":
-			continue
-		if chunk == "=" and prev and next:
-			command.kv_pairs.append(TrimsockCommand.pair_of(prev, next))
-		else:
-			command.params.append(chunk)
-	
-	# Calculate kv-map
-	if not command.kv_pairs.is_empty():
-		for pair in command.kv_pairs:
-			command.kv_map[pair.key] = pair.value
 
 func _init():
 	assert(false, "This class shouldn't be instantiated!")
--- trimsock.gd/tests/conventions.test.gd
+++ trimsock.gd/tests/conventions.test.gd
@@ -14,11 +14,8 @@ func suite():
 		check_type("should parse request", "cmd?1234 foo\n", TrimsockCommand.Type.REQUEST, "1234")
 		check_type("should parse success", ".1234 foo\n", TrimsockCommand.Type.SUCCESS_RESPONSE, "1234")
 		check_type("should parse error", "!1234 foo\n", TrimsockCommand.Type.ERROR_RESPONSE, "1234")
-		check_type("should parse stream", "cmd|1234 foo\n", TrimsockCommand.Type.STREAM_CHUNK, "1234")
-	)
-	
-	define("Params", func():
-		check_params("should parse params", "cmd foo bar\n", ["foo", "bar"], [])
+		check_type("should parse stream chunk", "cmd|1234 foo\n", TrimsockCommand.Type.STREAM_CHUNK, "1234")
+		check_type("should parse stream end", "cmd|1234\n", TrimsockCommand.Type.STREAM_FINSIH, "1234")
 	)
 
 func check_type(name: String, input: String, expected_type: TrimsockCommand.Type, expected_id: String) -> void:
@@ -29,13 +26,4 @@ func check_type(name: String, input: String, expected_type: TrimsockCommand.Type
 		expect_not_null(command)
 		expect_equal(command.type, expected_type)
 		expect_equal(command.exchange_id, expected_id)
-	)
-
-func check_params(name: String, input: String, expected_params: Array, expected_pairs: Array) -> void:
-	test(name, func():
-		reader.ingest_text(input)
-		
-		var command := reader.read()
-		expect_not_null(command)
-		expect_equal(command.params, expected_params)
 	)
