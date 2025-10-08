81535545534e81c9aba81cbdc769c5913a06f40d
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-08 13:48:23 +0200

    feat(gd): Raw commands (#50)
    Refs #29

---
+++ trimsock.gd/tests/reader.test.gd
@@ -0,0 +1,21 @@
+extends VestTest
+
+func get_suite_name():
+	return "Reader"
+
+var reader: TrimsockReader
+
+func suite():
+	on_case_begin.connect(func(__):
+		reader = TrimsockReader.new()
+	)
+	
+	test("should read raw message", func():
+		ok()
+		reader.ingest_text("\rcommand 4\na\ncd\n")
+		var command := reader.read()
+
+		expect_not_null(command)
+		expect_true(command.is_raw)
+		expect_equal(command.raw, "a\ncd".to_utf8_buffer())
+	)
