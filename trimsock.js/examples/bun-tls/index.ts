bdf93b22385e03bdd462122c5aaadfcc470ec0dc
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-27 18:51:30 +0100

    fmt

--- trimsock.js/examples/bun-tls/index.ts
+++ trimsock.js/examples/bun-tls/index.ts
@@ -2,19 +2,23 @@ import { BunSocketReactor } from "@foxssake/trimsock-bun";
 
 const reactor = new BunSocketReactor<void>()
   .on("echo", (cmd, xchg) => xchg.replyOrSend(cmd))
-  .on("password", async (cmd, xchg) => xchg.replyOrSend({ name: "password", text: await Bun.password.hash(cmd.text ?? "") }));
+  .on("password", async (cmd, xchg) =>
+    xchg.replyOrSend({
+      name: "password",
+      text: await Bun.password.hash(cmd.text ?? ""),
+    }),
+  );
 
 const port = 8893;
 reactor.listen({
   hostname: "localhost",
   port,
-  socket: {
-  },
+  socket: {},
   tls: {
     cert: Bun.file("./cert.pem"),
     key: Bun.file("./key.pem"),
-    passphrase: "supersecret"
-  }
+    passphrase: "supersecret",
+  },
 });
 
 console.log("Listening on port", port);
