9cfb722f249d12d0f0ff6615851c0a2a2c682b5a
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-28 22:38:28 +0100

    fmt

--- trimsock.js/packages/trimsock-js/perf/commands.perf.ts
+++ trimsock.js/packages/trimsock-js/perf/commands.perf.ts
@@ -1,18 +1,34 @@
 import { Command, type CommandSpec } from "@lib/command.js";
-import { run, bench, do_not_optimize } from "mitata";
+import { bench, do_not_optimize, run } from "mitata";
 
 let idx = 0;
 const commands: CommandSpec[] = [
   { name: "whereami", isRequest: true, requestId: "" },
-  { name: "session/set-game-id", isRequest: true, requestId: "", params: [ "RghrnyJK0mUA7cW05fCKo" ]},
-  { name: "lobby/create", isRequest: true, requestId: "", params: [ "noray://example.com:8890/Q9VKjXiAlwVK" ], kvParams: [["name", "Cool Lobby"]]},
+  {
+    name: "session/set-game-id",
+    isRequest: true,
+    requestId: "",
+    params: ["RghrnyJK0mUA7cW05fCKo"],
+  },
+  {
+    name: "lobby/create",
+    isRequest: true,
+    requestId: "",
+    params: ["noray://example.com:8890/Q9VKjXiAlwVK"],
+    kvParams: [["name", "Cool Lobby"]],
+  },
   { name: "lobby/lock", isRequest: true, requestId: "", text: "YuDqpQovXvpc" },
-  { name: "lobby/delete", isRequest: true, requestId: "", params: [ "YuDqpQovXvpc" ]}
+  {
+    name: "lobby/delete",
+    isRequest: true,
+    requestId: "",
+    params: ["YuDqpQovXvpc"],
+  },
 ];
 
 bench("serialize commands", () => {
-  do_not_optimize(Command.serialize(commands[idx]))
-  idx = (idx + 1) % commands.length
-})
+  do_not_optimize(Command.serialize(commands[idx]));
+  idx = (idx + 1) % commands.length;
+});
 
 await run();
--- trimsock.js/packages/trimsock-js/perf/reader.perf.ts
+++ trimsock.js/packages/trimsock-js/perf/reader.perf.ts
@@ -1,20 +1,20 @@
 import { TrimsockReader } from "@lib/reader.js";
-import { run, bench, do_not_optimize } from "mitata";
+import { bench, do_not_optimize, run } from "mitata";
 
 const reader = new TrimsockReader();
-let idx = 0
+let idx = 0;
 const commands = [
   "whereami?\n",
   "session/set-game-id RghrnyJK0mUA7cW05fCKo\n",
-  "lobby/create? noray://example.com:8890/Q9VKjXiAlwVK name=\"Cool Lobby\"\n",
+  'lobby/create? noray://example.com:8890/Q9VKjXiAlwVK name="Cool Lobby"\n',
   "lobby/lock? YuDqpQovXvpc",
-  "lobby/delete? YuDqpQovXvpc"
-]
+  "lobby/delete? YuDqpQovXvpc",
+];
 
 bench("ingest and parse", () => {
   reader.ingest(commands[idx]);
   do_not_optimize([...reader.commands()]);
   idx = (idx + 1) & commands.length;
-})
+});
 
 await run();
